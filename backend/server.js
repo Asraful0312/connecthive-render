3e53bb243b99e8e8ab21e4136827413a01e4312d
asraful islam <asrafulislamrayhan2@gmail.com>
2025-01-08 21:22:10 +0600

    edited server.js file and vite config file

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //middlewares
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -45,9 +45,8 @@ cloudinray.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
 });
 
 app.listen(PORT, () => console.log("listening on port", PORT));
